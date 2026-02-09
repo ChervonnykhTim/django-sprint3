bb0f4ba78fb7d5c5066de5b3dcbf3bf92f8def35
ChervonnykhTim <chervonnykh.tim@gmail.com>
2026-02-09 18:03:35 +0300

    Initial

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,41 +1,27 @@
-from django.contrib.admin import AdminSite
 from django.contrib import admin
 from .models import Category, Location, Post
 
 
-class MyAdminSite(AdminSite):
-    site_header = 'Администрирование Блога'
-    site_title = 'Мой сайт администрирования'
-    index_title = 'Добро пожаловать в админ-панель'
-
-
-admin_site = MyAdminSite()
+admin.site.site_header = 'Администрирование Блога'
+admin.site.site_title = 'Мой сайт администрирования'
+admin.site.index_title = 'Добро пожаловать в админ-панель'
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = (
-        'title',
-        'pub_date',
-        'author',
-        'category',
-        'location',
-        'is_published',
-    )
-    list_editable = (
-        'is_published',
-        'category',
-    )
+    list_display = ('title', 'pub_date', 'author', 'category', 'location',
+                    'is_published')
+    list_editable = ('is_published', 'category')
     search_fields = ('title', 'text')
 
 
-@admin.register(Category, site=admin_site)
+@admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug', 'is_published',)
     list_editable = ('is_published',)
 
 
-@admin.register(Location, site=admin_site)
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ('name', 'is_published',)
     list_editable = ('is_published',)
